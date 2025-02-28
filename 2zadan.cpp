891c78bb8dc0d05fe5230b5a01ce316f6b065fcb
Protochill <prostochillit@gmail.com>
2025-02-28 23:51:28 +0300

    Add files via upload

---
+++ 2zadan.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+    // вычисляем количество плит вдоль стороны n и m
+    long long dlin = (n + a - 1) / a;
+    long long shir = (m + a - 1) / a;
+    // общее количество плит
+    long long nado = dlin * shir;
+    cout << nado << endl;
+    return 0;
+}
